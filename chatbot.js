50910d5c5d50cdb3e4cbb0b84f95d8479c1a5ab9
hefaj1717 <karolwlosek06@gmail.com>
2023-10-29 14:21:20 +0100

    added age guesser

--- chatbot.js
+++ chatbot.js
@@ -9,7 +9,12 @@ let name = input();
 console.log("What a great name you have, " + name + "!");
 console.log("Let me guess your age.");
 console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+let remainder3 = input();
+let remainder5 = input();
+let remainder7 = input();
+
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
 // reading all remainders
 
-console.log("Your age is {your_age}; that's a good time to start programming!");
+console.log("Your age is " + age + " that's a good time to start programming!");
